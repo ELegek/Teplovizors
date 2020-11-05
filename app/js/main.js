42fbbb2aa7647102fe879bee491b48ecbc481234
Evgeny <evgeny.legek@yandex.ru>
2020-11-05 14:39:48 +0300

    categories v2

--- app/js/main.js
+++ app/js/main.js
@@ -28,6 +28,33 @@ $(function () {
         fade: true,
     });
 
+    $('.categories-slider').slick({
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+    });
+
 
 
 });
+
+document.querySelectorAll('.tabs-triggers__item').forEach((item) =>
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = e.target.getAttribute('href').replace('#', '');
+
+        document.querySelectorAll('.tabs-triggers__item').forEach(
+            (child) => child.classList.remove('tabs-triggers__item--active')
+        );
+        document.querySelectorAll('.tabs-content__item ').forEach(
+            (child) => child.classList.remove('tabs-content__item--active')
+        );
+
+        item.classList.add('tabs-triggers__item--active');
+        document.getElementById(id).classList.add('tabs-content__item--active');
+    })
+);
+
+document.querySelector('.tabs-triggers__item').click();
