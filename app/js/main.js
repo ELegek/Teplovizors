aaa7e45c39f96843e876b4d4807d83c28239b973
Evgeny <evgeny.legek@yandex.ru>
2020-11-15 21:31:38 +0300

    <div class="advantages-item">             <div class="advantages-icon">               <img src="" alt="">             </div>             <h4 class="advantages-subtitle">Быстрая доставка</h4>             <p class="advantages-text">У нас собственная курьерская служба по Москве, поэтому возможна доставка в день обращения! Доставка по России осуществляется с помощью транспортных компаний</p>

--- app/js/main.js
+++ app/js/main.js
@@ -129,3 +129,27 @@ document.querySelectorAll('.select').forEach(select => { //Выбриаем вс
     })
 
 })
+
+
+// Форма обратной связи 
+
+$(document).ready(function () {
+
+    //E-mail Ajax Send
+    $("form").submit(function () { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function () {
+            alert("Thank you!");
+            setTimeout(function () {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+});
