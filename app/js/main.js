dc46b283b5ad77c4041008180e35bc2dc712dc4c
Evgeny <evgeny.legek@yandex.ru>
2020-12-18 23:51:56 +0300

    myorders page

--- app/js/main.js
+++ app/js/main.js
@@ -90,6 +90,22 @@ $(function () {
         $('.catalog__inner-item').addClass('catalog__inner-item--list');
     });
 
+    // Аккордеон
+
+    $(function () {
+        // hide all content
+        $('.accordeon_content').hide();
+
+        $('.accordeon_title').click(function () {
+            $(this).parent().toggleClass('active').siblings().removeClass('active');
+            $('.accordeon_content').slideUp();
+
+            if (!$(this).next().is(":visible")) {
+                $(this).next().slideDown();
+            }
+        });
+    });
+
 
 
 
